9702a41f103e239d58cafe6d0513400dc8319a5e
Aniket Munshi <Aniket Munshi>
2016-12-27 18:02:09 +0530

    Init and other functionalities done

--- app.js
+++ app.js
@@ -11,40 +11,33 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginapp');
+mongoose.connect('mongodb://localhost/cbdb');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-// Init App
 var app = express();
 
-// View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
-// BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'web')));
 
-// Express Session
 app.use(session({
-    secret: 'secret',
+    secret: 'Anonymous was here!',
     saveUninitialized: true,
     resave: true
 }));
 
-// Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -62,14 +55,12 @@ app.use(expressValidator({
   }
 }));
 
-// Connect Flash
 app.use(flash());
 
-// Global Vars
 app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+  res.locals.success_msg = req.flash('Success!');
+  res.locals.error_msg = req.flash('Errror Occured!');
+  res.locals.error = req.flash('Error Encountered');
   res.locals.user = req.user || null;
   next();
 });
@@ -79,9 +70,7 @@ app.use(function (req, res, next) {
 app.use('/', routes);
 app.use('/users', users);
 
-// Set Port
-app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+app.listen(8081, function(){
+	console.log('Server running on http://localhost:', 8081);
 });
